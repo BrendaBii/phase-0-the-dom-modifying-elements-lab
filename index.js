3259a5ce254e647a2d3999e233e05efef2a568a3
BrendaBii <bii.brenda.bcb@gmail.com>
2023-06-12 16:16:12 +0300

    Removed and Created DOM objects with JavaScript.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+let main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = ("Brenda is the champion").toString();
+
+let body = document.getElementsByTagName("body");
+body.append(newHeader);
